6944525e3afb999c0ebdc916c1a683e18355d985
CareyWYR <wyr95626@163.com>
2018-07-02 16:47:53 +0800

    init commit

---
+++ user-dao/src/main/java/cn/leafw/zone/user/dao/UserDaoApplication.java
@@ -0,0 +1,12 @@
+package cn.leafw.zone.user.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDaoApplication.class, args);
+    }
+}
---
+++ user-web/src/main/java/cn/leafw/zone/user/UserWebApplication.java
@@ -0,0 +1,12 @@
+package cn.leafw.zone.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserWebApplication.class, args);
+    }
+}
